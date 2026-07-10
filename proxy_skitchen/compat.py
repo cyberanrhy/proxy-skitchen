96a8546e38b4d7205f4e8bb6a817f698cba11654
Stas <stas@users.noreply.github.com>
2026-07-10 15:12:42 +0300

    chore: bump version to 2.4.0

--- proxy_skitchen/compat.py
+++ proxy_skitchen/compat.py
@@ -79,7 +79,7 @@ else:
     ] if os.path.isdir(p)), os.path.expanduser("~"))
 
 APP_NAME = "proxy-skitchen"
-APP_VERSION = "2.2.0"
+APP_VERSION = "2.4.0"
 
 if IS_WINDOWS:
     CREATE_NO_WINDOW = 0x08000000
